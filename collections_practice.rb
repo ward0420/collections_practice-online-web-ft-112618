80d3fc84e1934bfeb727e49165c3f3f41faf6bd5
Don Ward <10is0420@gmail.com>
2018-12-08 14:27:15 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,36 @@
+def sort_array_asc(asc)
+  
+  asc.sort do | a , b |
+    
+    a <=> b
+    
+  end
+  
+end
 
+def sort_array_desc(desc)
+  
+  desc.sort do | a , b |
+    
+    b <=> a
+    
+  end
+  
+end
+  
+def sort_array_char_count(char)
+  
+  cap_char = []
+  
+  char.sort do | a , b | 
+    
+    a <=> b
+    
+      
+  cap_char << char.insert(0,char.delete_at(3))
+  
+  cap_char
+    
+  end
+
+end
